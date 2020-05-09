421c52522fd1288b1571fe439ba460087af1a831
Allohamora <herman2000@yandex.ru>
2020-05-09 21:05:47 +0300

    Fixed bug with invalid import

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from '../Button';
-import 'index.css';
+import './index.css';
 
 interface SelectorProps {
     changeHandler: (value: string) => void,
